ae6eed43a48d3cfa9f3f64cb991555ae4cf4cfd4
naman-gupta99 <kungfunaman@gmail.com>
2024-11-08 13:48:01 -0500

    Naman | Initial Commit

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class ProductionConfig(Config):
     
 class DevelopmentConfig(Config):
     WEBSOCKET_URL = "ws://localhost:5000"
-    SQLALCHEMY_DATABASE_URI = "postgresql://uf9vs1gr26qimf:p0162ed4e0e5cc49b0f9b916489987c0f508cf6c86dc99668e03a826186b4594c@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/degi8si8t791g2"
+    SQLALCHEMY_DATABASE_URI = "postgresql://flask_app:password@localhost:5432/cec_events"
